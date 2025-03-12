acb73ca83f7360067c70b7a589ffe7121eebce06
LanPet987 <115153511+LanPet987@users.noreply.github.com>
2025-03-12 11:27:10 +0100

    Órai munka

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mysql = require("mysql")
+
+
+
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createConnection({
+    user:"root",
+    host:"127.0.0.1",
+    port:3306,
+    password:"",
+    database:"felveteli"
+})
+
+app.get("/",(req,res)=>{
+    res.send("Működik a szerver")
+})
+
+app.get("/v",(req,res)=>{
+    res.send("Ez a v")
+})
+
+app.listen(3000,()=>{
+    console.log("A szerver a 3000 porton fut")
+})
